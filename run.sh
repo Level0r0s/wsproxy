53281560659f74a2890e3b9c03f71d54cf3a2fba
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-06 11:25:37 +0200

    Small change.

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cd $(dirname $0)
-docker run -d --name=wsproxy -p 80:80 -p 443:443 \
-           -v $(pwd):/data wsproxy
+docker create --name=wsproxy -p 80:80 -p 443:443 \
+              -v $(pwd):/data wsproxy
+docker start wsproxy
+
