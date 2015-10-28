66f21a8deb734f5a08abd2efc0b20f6b65eba643
Dashamir Hoxha <dashohoxha@gmail.com>
2015-10-28 08:28:00 +0100

    Start container automatcally when server is restarted.

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 cd $(dirname $0)
 docker create --name=wsproxy -p 80:80 -p 443:443 \
-              -v $(pwd):/data wsproxy
+              --restart=always -v $(pwd):/data wsproxy
 docker start wsproxy
 
